741b0716a987b49f3965f83bf08937c0b0a2dac8
Habib Kalia <hkalia@e1z1r3p1.42.us.org>
2016-07-30 22:16:08 -0700

    r02

--- r02/ex00/main.c
+++ r02/ex00/main.c
@@ -11,20 +11,51 @@
 /* ************************************************************************** */
 
 #include "rush_2.h"
+#include <stdio.h>
+
+int		find_x(char *str)
+{
+	int		x;
+
+	x = 0;
+	while (str[x] != '\n')
+		x++;
+	return (x);
+}
+
+int		find_y(char *str)
+{
+	int		y;
+	int		i;
+
+	y = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			y++;
+		i++;
+	}
+	return (y);
+}
 
 int		main(void)
 {
 	t_node	*head;
-	int		size;
 	char	buf;
 	char	*str;
+	int		x;
+	int		y;
 
 	head = NULL;
 	while (read(0, &buf, 1))
 		add_node_end(&head, buf);
-	size = ft_list_size(head);
-	str = (char*)malloc(sizeof(char) * size);
+	str = (char*)malloc(sizeof(char) * ft_list_size(head));
 	assign(str, head);
+	x = find_x(str);
+	y = find_y(str);
+	printf("%d\n", x);
+	printf("%d\n", y);
 	ft_putstr(str);
 	return (0);
 }
